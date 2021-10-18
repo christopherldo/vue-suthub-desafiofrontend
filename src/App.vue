3ec5ff420ad7846a2e7ea30d18618c1fb62e2362
Christopher de Oliveira <christopherforwork@gmail.com>
2021-10-17 22:41:52 -0400

    Started the project code with the header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-color: #222;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,78 @@
+.<template>
+  <header>
+    <div class="logo">
+      <a href="/">
+        <img src="../assets/logo.png" alt="Logo">
+      </a>
+    </div>
+    <nav class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/register">Registro</router-link>
+      <router-link to="/countries">Países</router-link>
+      <router-link to="/trade-blocs">Blocos Regionais</router-link>
+      <router-link to="/about">Sobre</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #111;
+    height: 80px;
+    border-bottom: 1px solid #444;
+
+    .logo {
+      img {
+        width: 250px;
+        cursor: pointer;
+        filter: drop-shadow(1px 1px 0px #666);
+      }
+    }
+
+    nav {
+      &:hover {
+        a {
+          opacity: 0.5;
+
+          &:hover {
+            opacity: 1;
+
+            &:not(&.router-link-exact-active) {
+              border-color: #fff;
+            }
+          }
+        }
+      }
+
+      a {
+        font-weight: bold;
+        color: #fff;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-family: Arial, Helvetica, sans-serif;
+        border-bottom: 1px solid transparent;
+        padding: 30px 0;
+        transition: all 0.3s ease-in-out;
+
+        &:not(:last-child) {
+          margin-right: 1rem;
+        }
+
+        &.router-link-exact-active {
+          color: #03d5c7;
+          border-color: #03d5c7;
+        }
+      }
+    }
+  }
+</style>
