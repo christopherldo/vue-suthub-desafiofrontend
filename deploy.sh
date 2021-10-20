3df43c11ea1a71ae764d443d15c958b10615beb4
Christopher de Oliveira <christopherforwork@gmail.com>
2021-10-19 23:59:48 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:christopherldo/vue-suthub-desafiofrontend.git master:gh-pages
+git push -f git@github.com:christopherldo/vue-suthub-desafiofrontend.git main:gh-pages
 cd - 
